66fc291cc86f056c6de6f0cbde33000ece229c25
Mikael Kalms <mikael@kalms.org>
2020-01-06 22:19:31 +0100

    Break vasmm68k formula

--- Formula/vasmm68k.rb
+++ Formula/vasmm68k.rb
@@ -1,7 +1,7 @@
 class Vasmm68k < Formula
   desc "Cross-assembler for 680x0 processors / Amiga OS"
   homepage "http://sun.hasenbraten.de/vasm"
-  url "http://server.owl.de/~frank/tags/vasm1_8e.tar.gz"
+  url "http://server.owl.de/~frank/tags/vasm1_8k.tar.gz"
   version "1.8e"
   sha256 "5f1ebb8b81d2d9664e5c2646f1abbc5fb795642ed117f55e84a66590a62760ff"
   version_scheme 1
